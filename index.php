cec2e8c0a87da6a747bbb89599bb716d18ac4fa9
Keoabetswe <KeoNthite@gmail.com>
2020-10-14 22:38:38 +0200

    add student and lecturer buttons

--- index.php
+++ index.php
@@ -3,40 +3,26 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Dashboard</title>
+        <title>Index</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
-       <main> 
-        <nav class="nav">
-          <a href="index.html" class="nav__link">
-            <i class="material-icons nav__icon">dashboard</i>
-            <span class="nav__text">Dashboard</span>
-          </a>
-          <a href="profile.html" class="nav__link nav__link--active">
-            <i class="material-icons nav__icon">person</i>
-            <span class="nav__text">Profile</span>
-          </a>
-          <a href="device.html" class="nav__link">
-            <i class="material-icons nav__icon">devices</i>
-            <span class="nav__text">Devices</span>
-          </a>
-          <a href="privacy.html" class="nav__link">
-            <i class="material-icons nav__icon">lock</i>
-            <span class="nav__text">Privacy</span>
-          </a>
-          <a href="settings.html" class="nav__link">
-            <i class="material-icons nav__icon">settings</i>
-            <span class="nav__text">Settings</span>
-          </a>
-        </nav>
-       </main>
+    
 
-       <h1>Dashboard</h1>
+  <div class="header">
+  	<h2>QR Code Student Attendance Monitoring System</h2>
+  </div>
+
+  <div>
+    <form method="post" action="lecturerLogin.php">
+      <center><button href="lecturerLogin" style="width: 100px; height: 30px;">Lecturer</button>   
+      <button href="studentLogin" style="width: 100px; height: 30px;">Student</button></center>
+    </form>
+    </div>      
         
-        <script src="" async defer></script>
+    <script src="" async defer></script>
     </body>
 </html>
