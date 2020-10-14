d2a69b03be4fee1d52c2f743fa46ca6847f7ebed
Keoabetswe <KeoNthite@gmail.com>
2020-10-14 23:26:06 +0200

    change navbar camera icon to qr code

--- lecturerDashboard.php
+++ lecturerDashboard.php
@@ -13,16 +13,12 @@
       <main>       
         <nav class="nav">
           <a href="attendanceHistory.php" class="nav__link--active">
-            <i class="material-icons nav__icon">history</i>
-            <span class="nav__text">History</span>
+            <i class="material-icons nav__icon">qr_code</i>
+            <span class="nav__text">Generate Code</span>
           </a>
           <a href="dashboard.php" class="nav__link nav__link">
             <i class="material-icons nav__icon">dashboard</i>
             <span class="nav__text">Dashboard</span>
-          </a>
-          <a href="qrCodeScanner.php" class="nav__link">
-            <i class="material-icons nav__icon">camera</i>
-            <span class="nav__text">Scanner</span>
           </a>
         </nav>
        </main>
