e70ca2fc22faa6aa4025eb324c416183a294c2d5
Keoabetswe <KeoNthite@gmail.com>
2020-10-19 21:27:57 +0200

    add qr code variables and scan function

--- js/app.js
+++ js/app.js
@@ -1,77 +1,58 @@
-//General Array Function
-function MakeArray(n) {
-  this.length = n;
-  for (var i = 1; i <= n; i++) {
-    this[i] = 0;
+const qrcode = window.qrcode;
+
+const video = document.createElement("video");
+const canvasElement = document.getElementById("qr-canvas");
+const canvas = canvasElement.getContext("2d");
+
+const qrResult = document.getElementById("qr-result");
+const outputData = document.getElementById("outputData");
+const btnScanQR = document.getElementById("btn-scan-qr");
+
+let scanning = false;
+
+qrcode.callback = res => {
+  if (res) {
+    outputData.innerText = res;
+    scanning = false;
+
+    video.srcObject.getTracks().forEach(track => {
+      track.stop();
+    });
+
+    qrResult.hidden = false;
+    canvasElement.hidden = true;
+    btnScanQR.hidden = false;
   }
+};
+
+btnScanQR.onclick = () => {
+  navigator.mediaDevices
+    .getUserMedia({ video: { facingMode: "environment" } })
+    .then(function(stream) {
+      scanning = true;
+      qrResult.hidden = true;
+      btnScanQR.hidden = true;
+      canvasElement.hidden = false;
+      video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+      video.srcObject = stream;
+      video.play();
+      tick();
+      scan();
+    });
+};
+
+function tick() {
+  canvasElement.height = video.videoHeight;
+  canvasElement.width = video.videoWidth;
+  canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+
+  scanning && requestAnimationFrame(tick);
 }
 
-
-//Initialize Days of Week Array
-days = new MakeArray(7);
-days[0] = "Saturday"
-days[1] = "Sunday"
-days[2] = "Monday"
-days[3] = "Tuesday"
-days[4] = "Wednesday"
-days[5] = "Thursday"
-days[6] = "Friday"
-
-//Initialize Months Array
-months = new MakeArray(12);
-months[1] = "January"
-months[2] = "February"
-months[3] = "March"
-months[4] = "April"
-months[5] = "May"
-months[6] = "June"
-months[7] = "July"
-months[8] = "August"
-months[9] = "September"
-months[10] = "October"
-months[11] = "November"
-months[12] = "December"
-
-
-//Day of Week Function
-function compute(form) {
-
-  var val1 = Number(val1);
-  var val2 = Number(val2);
-  var val3 = Number(val3);
-
-
-  var val1 = parseInt(form.day.value, 10)
-
-  if ((val1 < 0) || (val1 > 31)) {
-    alert("Invalid Day, can't be greater than 31 😑😑")
+function scan() {
+  try {
+    qrcode.decode();
+  } catch (e) {
+    setTimeout(scan, 300);
   }
-  var val2 = parseInt(form.month.value, 10)
-  if ((val2 < 0) || (val2 > 12)) {
-    alert("Invalid month, can't be greater than 12 😑😑")
-  }
-  var val2x = parseInt(form.month.value, 10)
-  var val3 = parseInt(form.year.value, 10)
-  if (val3 < 1000) {
-    alert("You ought to be dead dude! 👎👻")
-  }
-  if (val2 == 1) {
-    val2x = 13;
-    val3 = val3 - 1
-  }
-  if (val2 == 2) {
-    val2x = 14;
-    val3 = val3 - 1
-  }
-  var val4 = parseInt(((val2x + 1) * 3) / 5, 10)
-  var val5 = parseInt(val3 / 4, 10)
-  var val6 = parseInt(val3 / 100, 10)
-  var val7 = parseInt(val3 / 400, 10)
-  var val8 = val1 + (val2x * 2) + val4 + val3 + val5 - val6 + val7 + 2
-  var val9 = parseInt(val8 / 7, 10)
-  var val0 = val8 - (val9 * 7)
-  form.result1.value = months[val2] + " " + form.day.value + ", " + form.year.value
-  form.result2.value = "Hurray!! You were born on " + days[val0]
 }
-
-         // end script -->
