6e91b6f1651ead07c0967b26a9595071889ebb3f
Keoabetswe <KeoNthite@gmail.com>
2020-10-19 21:26:12 +0200

    add form tag

--- lecturerLogin.php
+++ lecturerLogin.php
@@ -11,26 +11,25 @@
     </head>
     <body>
     
-
   <div class="header">
   	<h2>Lecturer Login</h2>
   </div>
 
-  <div>
-    <form method="post" action="lecturerLogin.php">
-        <?php include('errors.php'); ?>
-        <div class="input-group">
-            <label>Email</label>
-            <input type="text" name="lecturer_email" >
-        </div>
-        <div class="input-group">
-            <label>Password</label>
-            <input type="password" name="lecturer_password">
-        </div>
-        <div class="input-group">
-            <button type="submit" class="btn" name="lecturer_login">Login</button>
-        </div>
-    </form>
+    <div>
+        <form method="post" action="lecturerLogin.php">
+            <?php include('errors.php'); ?>
+            <div class="input-group">
+                <label>Email</label>
+                <input type="text" name="lecturer_email" >
+            </div>
+            <div class="input-group">
+                <label>Password</label>
+                <input type="password" name="lecturer_password">
+            </div>
+            <div class="input-group">
+                <button type="submit" class="btn" name="lecturer_login">Login</button>
+            </div>
+        </form>
     </div>      
         
     <script src="" async defer></script>
