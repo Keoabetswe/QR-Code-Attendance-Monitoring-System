9314979582f5bea66c2d6943239cb8030e0064f6
Keoabetswe <KeoNthite@gmail.com>
2020-10-25 23:43:46 +0200

    rename attendanceHistory.php to attendees.php

---
+++ attendees.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Dashboard</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+       <main>       
+        <nav class="nav">
+          <a href="attendanceHistory.php" class="nav__link--active">
+            <i class="material-icons nav__icon">history</i>
+            <span class="nav__text">History</span>
+          </a>
+          <a href="studentDashboard.php" class="nav__link nav__link">
+            <i class="material-icons nav__icon">dashboard</i>
+            <span class="nav__text">Dashboard</span>
+          </a>
+          <a href="qrCodeDecoder.php" class="nav__link">
+            <i class="material-icons nav__icon">qr_code</i>
+            <span class="nav__text">Scan Code</span>
+          </a>
+        </nav>
+       </main>
+
+      <div class="header">
+  	    <h2>Attendance History</h2>
+      </div>       
+
+
+        
+    <script src="" async defer></script>
+    </body>
+</html>
